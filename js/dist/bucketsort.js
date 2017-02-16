1a95335df47aa74399caa18a6eaff0f19dc35603
Aurélien Ooms <aurelien.ooms@gmail.com>
2017-02-16 11:35:46 +0100

    aureooms- > @aureooms/ : update js code

--- js/dist/bucketsort.js
+++ js/dist/bucketsort.js
@@ -157,10 +157,10 @@
 	if (typeof exports === 'object') {
 		definition(exports);
 	} else if (typeof define === 'function' && define.amd) {
-		define('aureooms-js-bucketsort', [], function () {
+		define('@aureooms/js-bucketsort', [], function () {
 			return definition({});
 		});
 	} else if (typeof window === 'object' && typeof window.document === 'object') {
 		definition(window['bucketsort'] = {});
-	} else console.error('unable to detect type of module to define for aureooms-js-bucketsort');
+	} else console.error('unable to detect type of module to define for @aureooms/js-bucketsort');
 })();
